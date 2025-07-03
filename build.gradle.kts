b7786baac1824457e994105e0ec93302320193ae
l-phan <l-phan@creationline.com>
2025-07-03 22:01:28 +0900

    add open api

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+val generatedResourcesDir = "${layout.buildDirectory.get()}/generated-resources"
+
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "1.9.25"
+	id("org.openapi.generator") version "7.4.0"
+}
+
+group = "com.loc"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.liquibase:liquibase-core")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("io.swagger.core.v3:swagger-annotations:2.2.15")
+	implementation("io.swagger.core.v3:swagger-models:2.2.15")
+}
+
+// OpenAPI Generator
+openApiGenerate {
+	inputSpec.set("$rootDir/src/main/resources/static/openapi.yaml")
+	generatorName.set("kotlin-spring")
+    modelPackage.set("com.loc.productservice.model")
+    outputDir.set("$generatedResourcesDir/openapi")
+
+    configOptions.put("useSpringBoot3", "true")
+    configOptions.put("dateLibrary", "java8")
+    configOptions.put("delegatePattern", "true")
+    configOptions.put("interfaceOnly", "true")
+    configOptions.put("modelMutable", "true")
+    configOptions.put("useTags", "true")
+    configOptions.put("enumPropertyNaming", "UPPERCASE")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDir("$generatedResourcesDir/openapi/src/main/kotlin")
+        }
+    }
+}
+
+tasks.compileKotlin {
+    dependsOn(tasks.openApiGenerate)
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
--- src/main/java/com/loc/microservices/product_service_spring_boot/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.loc.microservices.product_service_spring_boot.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.loc.microservices.product_service_spring_boot.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
